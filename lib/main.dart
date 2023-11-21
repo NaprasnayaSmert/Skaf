a566b8b069ba8446240f83d924105350f402ed5a
NaprasnayaSmert <Dima030305@mail.ru>
2023-11-21 23:26:23 +0300

    skaf

---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+void main() { runApp(MyApp()); }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    body: Flex(
+      direction: Axis.vertical,
+        children: [
+          Flexible(
+          child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  flex: 1,
+
+
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.black,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 300,
+                  child: Text(
+                    '1', style: TextStyle(color: Colors.red, fontSize: 35),
+                ))),
+
+                Flexible(
+                flex: 1,
+
+                child:Container(
+                  alignment: Alignment.center,
+                  color: Colors.red,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 300,
+                    child: Text(
+                        '2', style: TextStyle(color: Colors.yellow, fontSize: 35)
+                )))])),
+
+
+
+          Flexible(
+          flex: 1,
+
+              child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  flex: 1,
+
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.pink,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 70,
+                    child: Text(
+                        '3', style: TextStyle(color: Colors.grey, fontSize: 35)
+                ))),
+
+                Flexible(
+                flex: 1,
+
+
+                child:Container(
+                  alignment: Alignment.center,
+                  color: Colors.blue,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 70,
+                    child: Text(
+                        '4', style: TextStyle(color: Colors.white, fontSize: 35)
+                )))])),
+    Flexible(
+    flex: 1,
+
+          child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  flex: 1,
+
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.purple,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 70,
+                    child: Text(
+                        '5', style: TextStyle(color: Colors.purpleAccent, fontSize: 35)
+                ))),
+
+                Flexible(
+                  flex: 1,
+
+
+                child:Container(
+                  alignment: Alignment.center,
+                  color: Colors.grey,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 70,
+                    child: Text(
+                        '6', style: TextStyle(color: Colors.deepOrange, fontSize: 35)
+                )))])),
+
+      Flexible(
+        flex: 1,
+
+          child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Flexible(
+                  flex: 1,
+
+
+                child:Container(
+                  alignment: Alignment.center,
+                  color: Colors.yellowAccent,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 300,
+                    child: Text(
+                        '7', style: TextStyle(color: Colors.greenAccent, fontSize: 35)
+                ))),
+
+                Flexible(
+                  flex: 1,
+
+
+                child:Container(
+                  alignment: Alignment.center,
+                  color: Colors.green,
+                  margin: EdgeInsets.all(8),
+                  width: 100,
+                  height: 300,
+                    child: Text(
+                        '8', style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 35)
+                )))])),
+        ],
+      ),
+    );
+  }
+}
